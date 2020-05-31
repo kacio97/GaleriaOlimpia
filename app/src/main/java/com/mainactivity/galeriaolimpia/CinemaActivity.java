f42e9ff53accc924b6bd675ff699c2060a2062f2
kacio97 <kamilstepien53@gmail.com>
2020-05-31 18:41:48 +0200

    Fix dla cinemaActivity.

--- app/src/main/java/com/mainactivity/galeriaolimpia/CinemaActivity.java
+++ app/src/main/java/com/mainactivity/galeriaolimpia/CinemaActivity.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
-import java.util.zip.Inflater;
 
 public class CinemaActivity extends AppCompatActivity {
 
